88ee578d924305cadfe3e361f0b5b16c3423d636
suryassuryas <71268577+suryassuryas@users.noreply.github.com>
2024-07-07 23:29:32 +0530

    files updated with journey postion

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate');
+      if (entry.target.classList.contains('left')) {
+        entry.target.classList.add('animate-left');
+      } else {
+        entry.target.classList.add('animate-right');
+      }
+    } else {
+      entry.target.classList.remove('animate');
+      entry.target.classList.remove('animate-left', 'animate-right');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+const contentDivs = document.querySelectorAll('.content-ti');
+contentDivs.forEach((div) => {
+  observer.observe(div);
+});
